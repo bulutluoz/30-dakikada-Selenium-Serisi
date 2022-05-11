479fe58cb0311994be60f2c1615ea58c523309d3
bulutluoz <abbulutluoz@gmail.com>
2022-05-11 12:59:14 +0200

    30 dakikada maven proje olusturma

---
+++ src/test/java/video1/IlkClass.java
@@ -0,0 +1,17 @@
+package video1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IlkClass {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+
+
+
+    }
+}
