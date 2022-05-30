c8c742fcd8556bb4fd66a74ca1f7629df1b970d1
bulutluoz <abbulutluoz@gmail.com>
2022-05-30 09:55:46 +0200

    6.video eklendi

---
+++ src/test/java/video6_JUnitAssertions/C1_IfElseIleAssertion.java
@@ -0,0 +1,54 @@
+package video6_JUnitAssertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C1_IfElseIleAssertion {
+
+    WebDriver driver;
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+        // 1- https:www.amazon.com sayfasina gidin
+        driver.get("https:www.amazon.com");
+        //2- arama cubuguna “Nutella” yazdirin
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella");
+        //3- Nutella yazdiktan sonra ENTER’a basarak arama islemini yapin
+        aramaKutusu.submit();
+        //4- Bulunan sonuc yazisinin "Nutella" icerdigini test edelim
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+
+        String actualYaziStr= sonucYaziElementi.getText();
+        // 1-48 of 105 results for "Nutella"
+        String expectedBulunacakKelime="Kutella";
+
+        if (actualYaziStr.contains(expectedBulunacakKelime)){
+            System.out.println("Nutella testi PASSED");
+        } else {
+            System.out.println("Sonuc yazisi Nutella icermiyor" +
+                               "\nNutella testi FAILED");
+        }
+
+    }
+}
---
+++ src/test/java/video6_JUnitAssertions/C2_AssertionMethodlariIleTest.java
@@ -0,0 +1,55 @@
+package video6_JUnitAssertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C2_AssertionMethodlariIleTest {
+    WebDriver driver;
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+        // 1- https:www.amazon.com sayfasina gidin
+        driver.get("https:www.amazon.com");
+        //2- arama cubuguna “Nutella” yazdirin
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella");
+        //3- Nutella yazdiktan sonra ENTER’a basarak arama islemini yapin
+        aramaKutusu.submit();
+        //4- Bulunan sonuc yazisinin "Nutella" icerdigini test edelim
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+
+        String actualYaziStr= sonucYaziElementi.getText();
+        // 1-48 of 105 results for "Nutella"
+        String expectedBulunacakKelime="Kutella";
+
+        Assert.assertTrue(actualYaziStr.contains(expectedBulunacakKelime));
+
+        int aliYas=56;
+        int veliYas=68;
+        int emeklilikYasi=65;
+
+        }
+
+    }
+
