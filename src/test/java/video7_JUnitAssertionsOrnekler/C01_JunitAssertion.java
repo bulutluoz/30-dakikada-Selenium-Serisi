06967a165ce13a93d3c8c43e5d965df0429ff9c8
bulutluoz <abbulutluoz@gmail.com>
2022-06-03 23:26:02 +0200

    7.video

---
+++ src/test/java/video7_JUnitAssertionsOrnekler/C01_JunitAssertion.java
@@ -0,0 +1,53 @@
+package video7_JUnitAssertionsOrnekler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_JunitAssertion {
+
+    @Test
+    public void test01() {
+    //1. Launch browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+    //3. Verify that home page is visible successfully
+        WebElement logoElementi= driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        Assert.assertTrue(logoElementi.isDisplayed());
+    //4. Click on 'Signup / Login' button
+        WebElement signUpLinki= driver.findElement(By.xpath("//a[text()=' Signup / Login'] "));
+        signUpLinki.click();
+    //5. Verify 'Login to your account' is visible
+        WebElement loginYaziElementi= driver.findElement(By.xpath("//h2[text()='Login to your account']"));
+        Assert.assertTrue(loginYaziElementi.isDisplayed());
+    //6. Enter correct email address and password
+        WebElement emailAdresKutusu= driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+        emailAdresKutusu.sendKeys("ahmet@nehaber.com");
+        WebElement passwordKutusu= driver.findElement(By.xpath("//input[@data-qa='login-password']"));
+        passwordKutusu.sendKeys("12345");
+    //7. Click 'login' button
+        driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+    //8. Verify that 'Logged in as username' is visible
+        WebElement loggedInYazisi= driver.findElement(By.xpath("//a[text()=' Logged in as '] "));
+        Assert.assertTrue(loggedInYazisi.isDisplayed());
+    //9. Click 'Logout' button
+        driver.findElement(By.xpath("//a[text()=' Logout'] ")).click();
+    //10. Verify that user is navigated to login page
+        String expectedUrl="https://automationexercise.com/login";
+        String actualUrl= driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actualUrl);
+    //11. Close driver
+        driver.close();
+    }
+}
---
+++ src/test/java/video7_JUnitAssertionsOrnekler/C02_JunitAssertion.java
@@ -0,0 +1,43 @@
+package video7_JUnitAssertionsOrnekler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_JunitAssertion {
+    @Test
+    public void test01() {
+        //1. Launch browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        //3. Verify that home page is visible successfully
+        WebElement logoElementi= driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        Assert.assertTrue(logoElementi.isDisplayed());
+        //4. Click on 'Products' button
+        driver.findElement(By.xpath("//a[text()=' Products'] ")).click();
+        //5. Verify user is navigated to ALL PRODUCTS page successfully
+        String expectedUrl="https://automationexercise.com/products";
+        String actualUrl= driver.getCurrentUrl();
+        Assert.assertEquals(expectedUrl,actualUrl);
+        //6. Enter product name in search input and click search button
+        WebElement aramaKutusu= driver.findElement(By.xpath("//input[@id='search_product']"));
+        aramaKutusu.sendKeys("tshirt");
+        driver.findElement(By.xpath("//button[@id='submit_search']")).click();
+        //7. Verify 'SEARCHED PRODUCTS' is visible
+        WebElement aramaYapildiElementi= driver.findElement(By.xpath("//h2[text()='Searched Products']"));
+        Assert.assertTrue(aramaYapildiElementi.isDisplayed());
+        //8. close driver
+        driver.close();
+
+    }
+}
