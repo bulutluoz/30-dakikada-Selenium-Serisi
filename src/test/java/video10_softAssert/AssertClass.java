41368498a2a7151f245b2de4dce0ba5cf695bb4e
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-06-22 18:10:05 +0200

    10.video

---
+++ src/test/java/video10_softAssert/AssertClass.java
@@ -0,0 +1,19 @@
+package video10_softAssert;
+
+import org.testng.annotations.Test;
+
+public class AssertClass {
+    @Test
+    public void testAssert() {
+    //1. Launch browser
+    // 2. Navigate to url 'http://automationexercise.com'
+    //3. Verify that home page is visible successfully
+    //4. Click on 'Products' button
+    //5. Verify user is navigated to ALL PRODUCTS page successfully
+    //6. The products list is visible
+    //7. Click on 'View Product' of first product
+    //8. User is landed to product detail page
+    //9. Verify that detail detail is visible: product name, category,
+
+    }
+}
